0e822e79e11799c28d3e23dacae027f5de6f72ff
Rodgers Kishagha <mkishagha@gmail.com>
2019-08-30 11:43:07 +0300

    add app logger

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,18 +1,44 @@
-import winston from 'winston';
+import appRoot from 'app-root-path';
+import { createLogger, format, transports} from 'winston';
 
-const logger = winston.createLogger({
+const logOptions = {
+  file: {
+    level: 'info',
+    filename: `${appRoot}/logs/app.log`,
+    handleExceptions: true,
+    json: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    colorize: false,
+  },
+  console: {
+    level: 'debug',
+    handleExceptions: true,
+    json: false,
+    colorize: true,
+  },
+};
+
+const logger = createLogger({
   defaultMeta: { service: 'user-service' },
-  format: winston.format.json(),
+  format: format.json(),
   level: 'info',
   transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' })
-  ]
+    new transports.File(logOptions.file),
+    new transports.Console(logOptions.console)
+  ],
+  exitOnError: false,
 });
 
+export class LoggerStream {
+  public write(message: string) {
+     logger.info(message.substring(0, message.lastIndexOf('\n')));
+  }
+}
+
 if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple()
+  logger.add(new transports.Console({
+    format: format.simple()
   }));
 }
 

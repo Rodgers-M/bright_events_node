aee87784be73fde871f60528944ac6f807f41363
Rodgers Kishagha <mkishagha@gmail.com>
2019-10-22 01:51:04 +0300

    add user profile table

--- src/database/migrations/20191018161946_users.ts
+++ src/database/migrations/20191018161946_users.ts
@@ -1,15 +1,15 @@
 import * as Knex from 'knex';
 import { addTableTimestamps } from '../addTableTimestamps';
 
-const USERS_TABLE = 'users';
+const ACCOUNTS_TABLE = 'accounts';
+const PROFILES_TABLE = 'profiles';
 
 async function createUuidExtension(knex: Knex): Promise<void> {
   await knex.raw('create extension if not exists "uuid-ossp"');
 }
 
-async function createUsersTable(knex: Knex): Promise<void> {
-
-    await knex.schema.createTable(USERS_TABLE, (table: Knex.CreateTableBuilder): void => {
+async function createAccountsTable(knex: Knex): Promise<void> {
+    await knex.schema.createTable(ACCOUNTS_TABLE, (table: Knex.CreateTableBuilder): void => {
 
       table.string('id')
         .primary()
@@ -23,14 +23,42 @@ async function createUsersTable(knex: Knex): Promise<void> {
 
     });
 
-    await addTableTimestamps(knex, USERS_TABLE);
+    await addTableTimestamps(knex, ACCOUNTS_TABLE);
+}
+
+async function createProfileTable(knex: Knex): Promise<void> {
+  await knex.schema.createTable(PROFILES_TABLE, (table: Knex.CreateTableBuilder): void => {
+    table.string('id')
+      .primary();
+
+    table.string('account_id')
+      .references('id')
+      .inTable(ACCOUNTS_TABLE)
+      .unique()
+      .notNullable();
+
+    table.string('first_name')
+      .nullable();
+
+    table.string('last_name')
+      .nullable();
+
+    table.integer('phone_number')
+      .nullable();
+
+    table.string('bio')
+      .nullable();
+
+  });
 }
 
 export async function up(knex: Knex): Promise<any> {
   await createUuidExtension(knex);
-  await createUsersTable(knex);
+  await createAccountsTable(knex);
+  await createProfileTable(knex);
 }
 
 export async function down(knex: Knex): Promise<any> {
-  await knex.schema.dropTable(USERS_TABLE);
+  await knex.schema.dropTable(PROFILES_TABLE);
+  await knex.schema.dropTable(ACCOUNTS_TABLE);
 }

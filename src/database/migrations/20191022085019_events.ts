d8a128bf117b1ba6e5727681db71538081d4cbd1
Rodgers Kishagha <mkishagha@gmail.com>
2019-10-22 23:24:19 +0300

    add events and rsvp migration file

---
+++ src/database/migrations/20191022085019_events.ts
@@ -0,0 +1,65 @@
+import * as Knex from 'knex';
+
+const EVENTS_TABLE_NAME = 'events';
+const RSVPS_TABLE_NAME = 'rsvps';
+const ACCOUNTS_TABLE_NAME = 'accounts';
+
+export  async function createEventsTable(knex: Knex): Promise<void> {
+  await knex.schema.createTable(EVENTS_TABLE_NAME, (table: Knex.CreateTableBuilder): void => {
+    table.string('id')
+      .primary();
+
+    table.string('tittle')
+      .notNullable();
+
+    table.string('slug')
+      .notNullable()
+      .unique();
+
+    table.text('description')
+      .notNullable();
+
+    table.string('location')
+      .notNullable();
+
+    table.date('date')
+      .notNullable();
+
+    table.time('time')
+      .notNullable();
+
+    table.date('rsvp_end_date')
+      .nullable();
+  });
+}
+
+export  async function createRsvpsTable(knex: Knex): Promise<void> {
+  await knex.schema.createTable(RSVPS_TABLE_NAME, (table: Knex.CreateTableBuilder): void => {
+    table.string('account_id')
+      .references('id')
+      .inTable(ACCOUNTS_TABLE_NAME)
+      .notNullable();
+
+    table.string('event_id')
+      .references('id')
+      .inTable(EVENTS_TABLE_NAME)
+      .notNullable();
+
+    table.boolean('attended')
+      .notNullable()
+      .defaultTo(false);
+
+    table.unique(['event_id', 'account_id']);
+
+  });
+}
+
+export async function up(knex: Knex): Promise<any> {
+  await createEventsTable(knex);
+  await createRsvpsTable(knex);
+}
+
+export async function down(knex: Knex): Promise<any> {
+  await knex.schema.dropTable(RSVPS_TABLE_NAME);
+  await knex.schema.dropTable(EVENTS_TABLE_NAME);
+}

ddcd3fcdce85dcc047a97edcafde5e0a621e5f2a
Rodgers Kishagha <mkishagha@gmail.com>
2020-02-12 00:10:20 +0300

    add method to get token from headers

--- src/lib/helpers/jwtHelper.ts
+++ src/lib/helpers/jwtHelper.ts
@@ -4,9 +4,10 @@ interface JwtPayload {
   id: string;
 }
 
-interface JwtExpiration {
-  expiresIn: string;
-}
+export const createToken = (payload: JwtPayload, secretKey: string, expiresIn: string | number) =>
+  jwt.sign(payload, secretKey, { expiresIn});
 
-export const createToken = (payload: JwtPayload, secretKey: string, expiresIn: JwtExpiration) =>
-  jwt.sign(payload, secretKey, expiresIn);
+export const getTokenFromBearerString = (bearerString: string): string => {
+  const token = bearerString.split(' ')[1] ;
+  return token;
+};

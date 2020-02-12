b1dbedfc4b9611c69e33a1347707d4457ca861df
Rodgers Kishagha <mkishagha@gmail.com>
2020-02-12 20:47:19 +0300

    remove rest routes

--- src/index.route.ts
+++
@@ -1,9 +0,0 @@
-import { Application, Request, Response } from 'express';
-import { getUserRouter } from './domains/users/user.route';
-
-const applyRoutes = (app: Application) => {
-  app.use(getUserRouter());
-  app.use('*', (req: Request, res: Response) => res.status(404).json({ message: `Route ${req.method} ${req.url} not found`}));
-};
-
-export default applyRoutes;

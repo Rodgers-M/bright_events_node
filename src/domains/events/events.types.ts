b37520c7c9141cb4a8080fac78b8bf5285f53dad
Rodgers Kishagha <mkishagha@gmail.com>
2020-02-15 21:53:31 +0300

    remove grpahql types

--- src/domains/events/events.types.ts
+++ src/domains/events/events.types.ts
@@ -1,31 +1,4 @@
 import * as knex from 'knex';
-import { GraphQLObjectType, GraphQLString, GraphQLID } from 'graphql';
-import { UserType, AccountLookupKey } from '../users/user.types';
-import { UserResource } from '../users/user.resource';
-
-// graphql types
-export const CreateEventType = new GraphQLObjectType({
-  name: 'CreateEvent',
-  fields: () => ({
-    id: { type: GraphQLID },
-    title: { type: GraphQLString },
-    slug: { type: GraphQLString },
-    description: { type: GraphQLString },
-    location: { type: GraphQLString },
-    date: { type: GraphQLString },
-    time: { type: GraphQLString },
-    rsvpEndDate: { type: GraphQLString },
-    createdBy: {
-      type: UserType,
-      async resolve(parent: any, args: any) {
-        const eventAuthor = UserResource.getUser(AccountLookupKey.ID, parent.createdBy);
-        return eventAuthor;
-      }
-    },
-  })
-});
-
-// interfaces
 export interface CreateEventBody {
   title: string;
   description: string;
--- src/domains/users/user.types.ts
+++ src/domains/users/user.types.ts
@@ -1,32 +1,3 @@
-import { GraphQLObjectType, GraphQLString, GraphQLID } from 'graphql';
-
-// graphql types
-export const UserType = new GraphQLObjectType({
-  name: 'User',
-  fields: () => ({
-    id: { type: GraphQLID },
-    email: { type: GraphQLString },
-    password: { type: GraphQLString },
-    createdAt: { type: GraphQLString }
-  })
-});
-
-export const CreatedUserType = new GraphQLObjectType({
-  name: 'CreatedUser',
-  fields: () => ({
-    token: { type: GraphQLString },
-  })
-});
-
-export const LoginUserType = new GraphQLObjectType({
-  name: 'LoginUser',
-  fields: () => ({
-    token: { type: GraphQLString },
-    message: { type: GraphQLString }
-  })
-});
-
-// interfaces
 export interface AccountBody {
   email: string;
   password: string;
